d794ab798d9be7775c48eebab1bbbe66acec60b1
drowe1 <skidrowe98@hotmail.com>
2017-02-10 10:27:51 -0600

    Did investments project

---
+++ compound/InvestmentsRunner.java
@@ -0,0 +1,11 @@
+package compound;
+
+public class InvestmentsRunner {
+	public static void main(String[] args){
+		investments test = new investments();
+		test.growth();
+		test.taxes();
+		test.afterRetirement();
+	}
+
+}
